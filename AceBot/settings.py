3359bfc075db400ebf4efabcf0e1ac98e08650b4
장경재 <secrecy418@naver.com>
2019-02-12 17:27:08 +0900

    config 경로 수정

--- AceBot/settings.py
+++ AceBot/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import config.config as con
+import botConfig.config as con
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
--- AceBot/wsgi.py
+++ AceBot/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for AceBot project.
+WSGI botConfig for AceBot project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
