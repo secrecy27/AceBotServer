51da5df8bb88ed72b5d97cc27262c843c051ed56
Whackur <ioumelon7@gmail.com>
2019-03-13 11:18:43 +0900

    봇이름을 안에 넣어야 응답하도록 변경

--- api/views.py
+++ api/views.py
@@ -9,59 +9,62 @@ from .serializers import MessageSerializer
 import os
 
 class MessageViewSet(viewsets.ModelViewSet):
-    # def check_call_bot(self, question):
-    #     bot_name = '엘라'
-    #     if ( bot_name in question ):
-    #         pass
-    #     else:
-    #         return "noData"
+    def check_call_bot(self, question):
+        bot_name = '엘라'
+        if ( bot_name in question ):
+            return True
+        else:
+            return False
 
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
 
     def process_request(self, question):
-        n = Naive_bayes()
-        high_class, high_score = n.classify(question)
-        print("question : ", question, "/ class : ", high_class, "/ score : ", high_score)
+        if self.check_call_bot(question) == True:
+            n = Naive_bayes()
+            high_class, high_score = n.classify(question)
+            print("question : ", question, "/ class : ", high_class, "/ score : ", high_score)
 
-        path = os.path.dirname(os.path.realpath(__file__))+'/../conversation'
+            path = os.path.dirname(os.path.realpath(__file__))+'/../conversation'
 
-        # print(path)
+            # print(path)
 
-        # className에 카테고리 추가, conversation 디렉터리안의 파일로 참조
+            # className에 카테고리 추가, conversation 디렉터리안의 파일로 참조
 
-        className = []
-        for fileName in os.listdir(path):
-            className.append(fileName)
-        # className = ["daios", "member", "private_sale", "homepage", "greeting", "morning",
-        #              "lunch", "congratulations", "laugh", "ella",
-        #              "whitepaper", "event", "advertising", "weather", "dirtcast"]
-        condition = {"whitepaper": {"word": "백서"}, "event": {"word": "이벤트"},
-                     "advertising": {"length": "100", "word": "http"}}
+            className = []
+            for fileName in os.listdir(path):
+                className.append(fileName)
+            # className = ["daios", "member", "private_sale", "homepage", "greeting", "morning",
+            #              "lunch", "congratulations", "laugh", "ella",
+            #              "whitepaper", "event", "advertising", "weather", "dirtcast"]
+            condition = {"whitepaper": {"word": "백서"}, "event": {"word": "이벤트"},
+                         "advertising": {"length": "100", "word": "http"}}
 
-        if high_class in className:
-            if high_class in condition:
-                for k in condition[high_class].keys():
-                    # k = word, length
-                    if k in "length":
-                        if len(question) > int(condition[high_class][k]):
-                            return self.process_answer(high_class)
+            if high_class in className:
+                if high_class in condition:
+                    for k in condition[high_class].keys():
+                        # k = word, length
+                        if k in "length":
+                            if len(question) > int(condition[high_class][k]):
+                                return self.process_answer(high_class)
+                            else:
+                                return "noData"
                         else:
-                            return "noData"
-                    else:
-                        if condition[high_class][k] in question:
-                            return self.process_answer(high_class)
-                        else:
-                            return "noData"
-                        # print("11",condition[high_class][k])
-                        # if str(i for i in condition[high_class][k]) in question:
-                        #     print("있음")
-                        # else:
-                        #     print("here : ",(i for i in condition[high_class][k]))
-                        #     print("없음")
+                            if condition[high_class][k] in question:
+                                return self.process_answer(high_class)
+                            else:
+                                return "noData"
+                            # print("11",condition[high_class][k])
+                            # if str(i for i in condition[high_class][k]) in question:
+                            #     print("있음")
+                            # else:
+                            #     print("here : ",(i for i in condition[high_class][k]))
+                            #     print("없음")
+                else:
+                    return self.process_answer(high_class)
             else:
-                return self.process_answer(high_class)
-        else:
+                return "noData"
+        else :
             return "noData"
 
     @staticmethod
